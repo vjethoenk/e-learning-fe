27efe9f06ba407eaf512daaf29c0dbbab97112fb
frontend_cmd <developer@innofly-software.com>
2025-09-23 08:44:37 +0700

    update

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -68,7 +68,14 @@ const AppHeader: React.FC = () => {
             </button>
 
             {/* Submenu */}
-            <div className="absolute left-0 mt-2 w-48 bg-white border rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-200 z-50">
+            <div
+              className="
+      absolute left-0 mt-2 w-48 bg-white border rounded-lg shadow-lg 
+      opacity-0 pointer-events-none 
+      group-hover:opacity-100 group-hover:pointer-events-auto 
+      transition duration-200 z-50
+    "
+            >
               <a
                 href="#"
                 className="block px-4 py-2 text-gray-700 hover:bg-red-50 hover:text-red-600"
